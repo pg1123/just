86b9c9dd5be3ed3034fc37e2591bedf6451271de
Yang Liu <yang.liu@mdpi.com>
2019-05-02 11:56:26 +0800

    1

--- e/data/tmp/tempnews1_all.php
+++ e/data/tmp/tempnews1_all.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+if(!defined('InEmpireCMS'))
+{
+	exit();
+}
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title> Lynda - SOLIDWORKS - Tools Suite - Powered by EmpireCMS</title>
-<meta name="keywords" content="" />
-<meta name="description" content=" Lynda - SOLIDWORKS - Tools Suite" />
+<title><?=$grpagetitle?> - Powered by EmpireCMS</title>
+<meta name="keywords" content="<?=$ecms_gr[keyboard]?>" />
+<meta name="description" content="<?=$grpagetitle?>" />
 <link href="/skin/default/css/style.css" rel="stylesheet" type="text/css" />
 <script type="text/javascript" src="/skin/default/js/tabs.js"></script>
 <script type="text/javascript" src="/e/data/js/ajax.js"></script>
@@ -23,7 +28,7 @@ document.write('<script src="/e/member/login/loginjs.php?t='+Math.random()+'"><'
 </script>
 </td>
 <td align="right">
-<a onclick="window.external.addFavorite(location.href,document.title)" href="#ecms">加入收藏</a> | <a onclick="this.style.behavior='url(#default#homepage)';this.setHomePage('/')" href="#ecms">设为首页</a> | <a href="/e/member/cp/">会员中心</a> | <a href="/e/DoInfo/">我要投稿</a> | <a href="/e/web/?type=rss2&classid=4" target="_blank">RSS<img src="/skin/default/images/rss.gif" border="0" hspace="2" /></a>
+<a onclick="window.external.addFavorite(location.href,document.title)" href="#ecms">加入收藏</a> | <a onclick="this.style.behavior='url(#default#homepage)';this.setHomePage('/')" href="#ecms">设为首页</a> | <a href="/e/member/cp/">会员中心</a> | <a href="/e/DoInfo/">我要投稿</a> | <a href="/e/web/?type=rss2&classid=<?=$ecms_gr[classid]?>" target="_blank">RSS<img src="/skin/default/images/rss.gif" border="0" hspace="2" /></a>
 </td>
 </tr>
 </table>
@@ -41,39 +46,40 @@ document.write('<script src="/e/member/login/loginjs.php?t='+Math.random()+'"><'
   <tr> 
     <td class="nav_global"><ul>
         <li class="curr" id="tabnav_btn_0" onmouseover="tabit(this)"><a href="/">首页</a></li>
-        <li id="tabnav_btn_1" onmouseover="tabit(this)"><a href="/jiaocheng/">教程</a></li> </ul></td>
+        <? @sys_ShowClassByTemp('0',12,0,0);?> </ul></td>
   </tr>
 </table>
 <table width="100%" border="0" cellspacing="10" cellpadding="0">
 <tr valign="top">
 <td class="main"><table width="100%" border="0" cellspacing="0" cellpadding="0" class="position">
 <tr>
-<td>您当前的位置1：<a href="/">首页</a>&nbsp;>&nbsp;<a href="/jiaocheng/">教程</a>&nbsp;>&nbsp;<a href="/jiaocheng/3djiaocheng/">3D教程</a>&nbsp;>&nbsp;<a href="/jiaocheng/3djiaocheng/3d1/">3d-1</a></td>
+<td>您当前的位置1：<?=$grurl?></td>
 </tr>
 </table>
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="box">
 <tr>
 <td><table width="100%" border="0" cellpadding="0" cellspacing="0" class="title_info">
 <tr>
-<td><h1> Lynda - SOLIDWORKS - Tools Suite</h1></td>
+<td><h1><?=$ecms_gr[title]?></h1></td>
 </tr>
 <tr>
-<td class="info_text">时间：2019-05-01 15:54:25&nbsp;&nbsp;来源：&nbsp;&nbsp;作者：</td>
+<td class="info_text">时间：<?=date('Y-m-d H:i:s',$ecms_gr[newstime])?>&nbsp;&nbsp;来源：<?=$docheckrep[1]?ReplaceBefrom($ecms_gr[befrom]):$ecms_gr[befrom]?>&nbsp;&nbsp;作者：<?=$docheckrep[2]?ReplaceWriter($ecms_gr[writer]):$ecms_gr[writer]?></td>
 </tr>
 </table>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
 <tr>
-<td id="text">Lynda - SOLIDWORKS - Tools Suite<br /><br />Lynda - SolidWorks工具套件教程<br /><br /><img alt="" src="/d/file/p/2019/05-01/0fb9637bffa111aa249d92142cc49087.jpg" style="width: 880px; height: 495px;" /><p align="center" class="pageLink"></p></td>
+<td id="text"><?=strstr($ecms_gr[newstext],'[!--empirenews.page--]')?'[!--newstext--]':$ecms_gr[newstext]?>
+<p align="center" class="pageLink">[!--page.url--]</p></td>
 </tr>
 </table>
 <table border="0" align="center" cellpadding="0" cellspacing="8">
 <tr>
 <td><table border="0" align="center" cellpadding="0" cellspacing="0" class="digg">
 <tr>
-<td class="diggnum" id="diggnum"><strong><script type="text/javascript" src="/e/public/ViewClick/?classid=4&id=1&down=5"></script></strong></td>
+<td class="diggnum" id="diggnum"><strong><script type="text/javascript" src="/e/public/ViewClick/?classid=<?=$ecms_gr[classid]?>&id=<?=$ecms_gr[id]?>&down=5"></script></strong></td>
 </tr>
 <tr>
-<td class="diggit"><a href="JavaScript:makeRequest('/e/public/digg/?classid=4&id=1&dotop=1&doajax=1&ajaxarea=diggnum','EchoReturnedText','GET','');">来顶一下</a></td>
+<td class="diggit"><a href="JavaScript:makeRequest('/e/public/digg/?classid=<?=$ecms_gr[classid]?>&id=<?=$ecms_gr[id]?>&dotop=1&doajax=1&ajaxarea=diggnum','EchoReturnedText','GET','');">来顶一下</a></td>
 </tr>
 </table></td>
 <td><table border="0" align="center" cellpadding="0" cellspacing="0" class="digg">
@@ -107,7 +113,7 @@ document.write('<script src="/e/member/login/loginjs.php?t='+Math.random()+'"><'
 <td><table width="100%" border="0" cellpadding="0" cellspacing="0" class="title">
 <tr>
 <td><strong>发表评论</strong></td>
-<td align="right"><a href="/e/pl/?classid=4&amp;id=1">共有<span><script type="text/javascript" src="/e/public/ViewClick/?classid=4&id=1&down=2"></script></span>条评论</a></td>
+<td align="right"><a href="/e/pl/?classid=<?=$ecms_gr[classid]?>&amp;id=<?=$ecms_gr[id]?>">共有<span><script type="text/javascript" src="/e/public/ViewClick/?classid=<?=$ecms_gr[classid]?>&id=<?=$ecms_gr[id]?>&down=2"></script></span>条评论</a></td>
 </tr>
 </table>
 <table width="100%" border="0" cellspacing="10" cellpadding="0">
@@ -128,11 +134,11 @@ document.write('<script src="/e/member/login/loginjs.php?t='+Math.random()+'"><'
 </tr>
 </table>
 <textarea name="saytext" rows="6" id="saytext"></textarea><input name="imageField" type="image" src="/e/data/images/postpl.gif"/>
-<input name="id" type="hidden" id="id" value="1" />
-<input name="classid" type="hidden" id="classid" value="4" />
+<input name="id" type="hidden" id="id" value="<?=$ecms_gr[id]?>" />
+<input name="classid" type="hidden" id="classid" value="<?=$ecms_gr[classid]?>" />
 <input name="enews" type="hidden" id="enews" value="AddPl" />
 <input name="repid" type="hidden" id="repid" value="0" />
-<input type="hidden" name="ecmsfrom" value="/jiaocheng/3djiaocheng/3d1/2019-05-01/1.html"></td>
+<input type="hidden" name="ecmsfrom" value="<?=$grtitleurl?>"></td>
 </tr>
 </table>
 </td>
@@ -146,7 +152,8 @@ document.write('<script src="/e/member/login/loginjs.php?t='+Math.random()+'"><'
 </table>
 <table width="100%" border="0" cellspacing="8" cellpadding="0" class="box">
 <tr>
-<td></td>
+<td><? @sys_GetClassNewsPic('news',2,4,128,90,1,20,20);?>
+</td>
 </tr>
 </table>
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="title margin_top">
@@ -157,7 +164,8 @@ document.write('<script src="/e/member/login/loginjs.php?t='+Math.random()+'"><'
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="box">
 <tr>
 <td><ul>
-无相关信息</ul></td>
+<?=GetKeyboard($ecms_gr[keyboard],$ecms_gr[keyid],$ecms_gr[classid],$ecms_gr[id],$class_r[$ecms_gr[classid]][link_num])?>
+</ul></td>
 </tr>
 </table>
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="title margin_top">
@@ -168,7 +176,7 @@ document.write('<script src="/e/member/login/loginjs.php?t='+Math.random()+'"><'
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="box">
 <tr>
 <td><ul>
-              <script src='/d/js/class/class4_newnews.js'></script></ul></td>
+              <script src='/d/js/class/class<?=$ecms_gr[classid]?>_newnews.js'></script></ul></td>
 </tr>
 </table>
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="title margin_top">
@@ -179,7 +187,7 @@ document.write('<script src="/e/member/login/loginjs.php?t='+Math.random()+'"><'
 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="box">
 <tr>
 <td><ul>
-              <script src='/d/js/class/class4_hotnews.js'></script></ul></td>
+              <script src='/d/js/class/class<?=$ecms_gr[classid]?>_hotnews.js'></script></ul></td>
 </tr>
 </table></td>
 </tr>
@@ -231,5 +239,6 @@ document.write('<script src="/e/member/login/loginjs.php?t='+Math.random()+'"><'
 </td>
 </tr>
 </table>
-<script src="/e/public/onclick/?enews=donews&classid=4&id=1"></script></body>
+<?='<script src="'.$public_r[newsurl].'e/public/onclick/?enews=donews&classid='.$ecms_gr[classid].'&id='.$ecms_gr[id].'"></script>'?>
+</body>
 </html>
